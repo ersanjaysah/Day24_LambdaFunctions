b20398fa8e5a9a2418b64a2d8a4416b20abaa390
sanjay <ssah6694@gmail.com>
2022-04-10 10:03:24 +0530

    [sanjay]Added: welcome file.

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the lambda problems");
+        }
+    }
+}
