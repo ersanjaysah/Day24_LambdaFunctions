3cebb55b427798676f212fe430d6a28e239bedda
sanjay <ssah6694@gmail.com>
2022-04-10 10:49:25 +0530

    [sanjay]Added: Validation of user registration by using Lambda

--- LambdaExpression/LambdaExpression/LambdaFunction.cs
+++ LambdaExpression/LambdaExpression/LambdaFunction.cs
@@ -13,17 +13,17 @@ namespace LambdaExpression
         {
             string[] RegexPattern = new string[5];
 
-            RegexPattern[0] = "^[A-Z][A-Za-z]{2,}$";
+            RegexPattern[0] = "^[A-Z][A-Za-z]{2,}$"; // check user first name
 
-            RegexPattern[1] = "^[A-Z][A-Za-z]{2,}$";
+            RegexPattern[1] = "^[A-Z][A-Za-z]{2,}$"; // check user last name
 
-            RegexPattern[2] = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+            RegexPattern[2] = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";// validate Email id
 
-            RegexPattern[3] = "^[1-9]{2,}[ ][0-9]{10}$";
+            RegexPattern[3] = "^[1-9]{2,}[ ][0-9]{10}$";  //validate phone number
 
-            RegexPattern[4] = "^[A-Za-z0-9]*[!@#$%^$*]{1}([a-zA-Z0-9])*$";
+            RegexPattern[4] = "^[A-Za-z0-9]*[!@#$%^$*]{1}([a-zA-Z0-9])*$"; // validate password
 
-            string[] UserInput = new string[6];
+            string[] UserInput = new string[5];
 
             Console.WriteLine("Enter First Name:");
             UserInput[0] = Console.ReadLine();
@@ -42,7 +42,7 @@ namespace LambdaExpression
 
             for (int i = 0; i < 5; i++)
             {
-                Func<string, bool> Validater = x => Regex.IsMatch(x, RegexPattern[i]);
+                Func<string, bool> Validater = x => Regex.IsMatch(x, RegexPattern[i]); // using lambda expression
                 bool result = Validater(UserInput[i]);
                 Console.WriteLine(($"{i + 1} {UserInput[i]} : {result}"));
             }
--- LambdaExpression/LambdaExpression/Program.cs
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -7,7 +7,7 @@ namespace LambdaExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the lambda problems");
-            LambdaFunction lambda = new LambdaFunction();
+            LambdaFunction lambda = new LambdaFunction(); // calling
             lambda.Validate();
         }
     }
